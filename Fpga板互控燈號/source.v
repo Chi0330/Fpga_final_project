de5036ccfa4ece965e0e4d25d2103bc3fd0aed93
Chi0330 <114386862+Chi0330@users.noreply.github.com>
2023-01-18 18:44:58 +0800

    Update source.v

--- Fpga板互控燈號/source.v
+++ Fpga板互控燈號/source.v
@@ -1,25 +1,28 @@
 
 `timescale 1ns / 1ps
 
-module source(clk,rst,sw,Dinout,LED);
+module source(clk,rst,in,Dinout,LED);
 
-input clk,rst,sw;
-inout  Dinout;
-output reg LED;
+    input clk,rst,in;
+    inout  Dinout;
+    output reg LED;
 
-wire Dout,Din;
+    wire Dout,Din;
 
-assign Dout = 0 ;
+    assign Dout = 0 ;
 
-assign Dinout = sw ?Dout :1'bz;
-assign Din    = sw ?1'b1 :Dinout;
+    assign Dinout = in ?Dout :1'bz;
+    assign Din    = in ?1'b1 :Dinout;
 
-always @(*) begin
-    if(rst) LED <= 0;
-    else begin
-        if(Din == 0)  LED <= 1;
-        else     LED <= 0;
+    always @(*) begin
+        if(rst) 
+            LED <= 0;
+        else begin
+            if(Din == 0)  
+                LED <= 1;
+            else
+                LED <= 0;
+        end
     end
-end
 
 endmodule
